d57957ce4551d64d457437f71d41d0e814f5cf49
Michael Jensen <mcjensen24@gmail.com>
2021-05-14 16:25:33 -0400

    set up navbar link

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,40 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import "./index.css"
+
 
 const NavBar = () => {
     return (
         <div className="navbar-container">
-            <div className="navbar-content">
+            <div className="navbar-logo">
 
+            </div>
+            <div className="navbar-links">
+                <div className="link">
+                    <Link to={"home"}>
+                        <h1>Home</h1>
+                    </Link>
+                </div>
+                <div className="link">
+                    <Link to={"about-us"}>
+                        <h1>About Us</h1>
+                    </Link>
+                </div>
+                <div className="link">
+                    <Link to={"gallery"}>
+                        <h1>Gallery</h1>
+                    </Link>
+                </div>
+                <div className="link">
+                    <Link to={"process"}>
+                        <h1>Out Process</h1>
+                    </Link>
+                </div>
+                <div className="link">
+                    <Link to={"contact"}>
+                        <h1>Contact Us</h1>
+                    </Link>
+                </div>
             </div>
         </div >
     )
