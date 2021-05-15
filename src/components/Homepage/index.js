7f1f7dc0aa1ef954f3a9123457cf97a38f8880ac
Michael Jensen <mcjensen24@gmail.com>
2021-05-15 19:05:25 -0400

    created working basic design for homepage

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,7 +3,33 @@ import "./index.css"
 
 const Homepage = () => {
     return (
-        <h1>Homepage</h1>
+        <div className="homepage-container">
+            <div className="homepage-row">
+                <div className="homepage-column">
+                    <div className="home_container1">
+                        <img src="/images/homes/homeimage.jpeg" alt="home" className="home_image" />
+                    </div>
+                    <div className="home_container2">
+                        <img src="/images/homes/luxhome1.jpg" alt="home" className="home_image" />
+                    </div>
+                </div>
+                <div className="homepage-column">
+                    <div className="home_container2">
+                        <img src="/images/homes/luxhome2.jpg" alt="home" className="home_image" />
+                    </div>
+                    <div className="home_container1">
+                        <img src="/images/homes/luxhome3.jpg" alt="home" className="home_image" />
+                    </div>
+                </div>
+                <div className="home-intro">
+                    <img src="/images/homes/WH.jpg" alt="home" className="home-intro_content" />
+                    <div className="home-intro_images">
+                        <img src="/images/homes/luxhome4.jpg" alt="home" className="intro-image" />
+                        <img src="/images/homes/luxhome5.jpg" alt="home" className="intro-image" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
