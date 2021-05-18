f83ff20f7f4f0ce0074d70ba7432c2af9017c657
Michael Jensen <mcjensen24@gmail.com>
2021-05-17 23:56:25 -0400

    added columns of photos where rows were at bottom of page

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -77,13 +77,20 @@ const Homepage = () => {
                 </div>
             </div>
             <div className="homepage-row">
-                <div className="home-intro2">
-                    <div className="home-intro_images">
-                        <img src="/images/homes/luxhome4.jpg" alt="home" className="intro-image2" />
-                        <img src="/images/homes/luxhome5.jpg" alt="home" className="intro-image2" />
+                <div className="homepage-column">
+                    <div className="home_container2">
+                        <img src="/images/homes/luxhome1.jpg" alt="home" className="home_image" />
                     </div>
-                    <div className="content_container2">
-                        <img src="/images/homes/WH.jpg" alt="home" className="home-intro_content2" />
+                    <div className="home_container1">
+                        <img src="/images/homes/homeimage.jpeg" alt="home" className="home_image" />
+                    </div>
+                </div>
+                <div className="homepage-column">
+                    <div className="home_container1">
+                        <img src="/images/homes/luxhome3.jpg" alt="home" className="home_image" />
+                    </div>
+                    <div className="home_container2">
+                        <img src="/images/homes/luxhome2.jpg" alt="home" className="home_image" />
                     </div>
                 </div>
                 <div className="home-intro3">
