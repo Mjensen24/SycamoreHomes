84f4cafdbc8b3dc1870b137b1ee35fef61e2e714
Michael Jensen <mcjensen24@gmail.com>
2021-05-19 16:39:02 -0400

    created basic layout for contact info on contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import "./index.css"
+import { GoLocation } from 'react-icons/go';
 
 const Contact = () => {
     return (
         <div className="contact-container">
             <div className="contact-info_container">
                 <h1>GET IN TOUCH</h1>
-                <div className="contact-info_column">
-
+                <div className="contact-column_container">
+                    <div className="contact-info_column left">
+                        <GoLocation />
+                        <div className="contact-info">
+                            <h3>ADDRESS</h3>
+                            <h4>Sycamore Custom Luxery Homes</h4>
+                            <p>1234 Rolling Hill Road <br /> Frederick MD. 21701</p>
+                        </div>
+                    </div>
+                    <div className="contact-info_column">
+                        <GoLocation />
+                        <div className="contact-info">
+                            <h3>PHONE</h3>
+                            <h4>Sycamore Custom Luxery Homes</h4>
+                            <p>1234 Rolling Hill Road <br /> Frederick MD. 21701</p>
+                        </div>
+                    </div>
+                    <div className="contact-info_column right">
+                        <GoLocation />
+                        <div className="contact-info">
+                            <h3>EMAIL</h3>
+                            <h4>Sycamore Custom Luxery Homes</h4>
+                            <p>1234 Rolling Hill Road <br /> Frederick MD. 21701</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="contact-info_column">
-
-                </div>
-                <div className="contact-info_column">
-
-                </div>
-
             </div>
         </div>
     )
