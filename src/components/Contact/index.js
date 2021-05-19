d17342a0efa0e22276a55a94542fcce10a12a3dd
Michael Jensen <mcjensen24@gmail.com>
2021-05-19 16:56:18 -0400

    finished top banner for contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./index.css"
 import { GoLocation } from 'react-icons/go';
+import { HiPhone, HiOutlineMail } from 'react-icons/hi';
 
 const Contact = () => {
     return (
@@ -9,27 +10,27 @@ const Contact = () => {
                 <h1>GET IN TOUCH</h1>
                 <div className="contact-column_container">
                     <div className="contact-info_column left">
-                        <GoLocation />
+                        <HiPhone />
                         <div className="contact-info">
-                            <h3>ADDRESS</h3>
-                            <h4>Sycamore Custom Luxery Homes</h4>
-                            <p>1234 Rolling Hill Road <br /> Frederick MD. 21701</p>
+                            <h3>PHONE</h3>
+                            <h4>Bernice Printz <br /> Co-Owner/Designer</h4>
+                            <p>301-748-1614</p>
                         </div>
                     </div>
                     <div className="contact-info_column">
                         <GoLocation />
                         <div className="contact-info">
-                            <h3>PHONE</h3>
-                            <h4>Sycamore Custom Luxery Homes</h4>
+                            <h3>ADDRESS</h3>
+                            <h4>Sycamore Custom Luxery <br /> Homes</h4>
                             <p>1234 Rolling Hill Road <br /> Frederick MD. 21701</p>
                         </div>
                     </div>
                     <div className="contact-info_column right">
-                        <GoLocation />
+                        <HiOutlineMail />
                         <div className="contact-info">
                             <h3>EMAIL</h3>
-                            <h4>Sycamore Custom Luxery Homes</h4>
-                            <p>1234 Rolling Hill Road <br /> Frederick MD. 21701</p>
+                            <h4>Bernice Printz <br /> Co-Owner/Designer</h4>
+                            <p>Baberni2@aol.com</p>
                         </div>
                     </div>
                 </div>
