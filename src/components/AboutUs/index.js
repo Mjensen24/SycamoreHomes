510710dc71687bd715fde35203530d622b8b3bd5
Michael Jensen <mcjensen24@gmail.com>
2021-05-21 19:53:22 -0400

    have a basic structure to about us p page

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -3,7 +3,29 @@ import "./index.css"
 
 const AboutUs = () => {
     return (
-        <h1>about us</h1>
+        <div className="about-container">
+            <div className="about-row">
+                <div className="about-header_container">
+                    <h4>About Us</h4>
+                    <h1>With 30 years of construction under our belt, we are dedicated to building quality homes while working within your budget. Your happiness is our success.</h1>
+                </div>
+            </div>
+            <div className="about-background"></div>
+            <div className="about-row_2">
+                <div className="about-column_1">
+                    <div className="column1-info_container">
+
+                    </div>
+                    <div className="column1-info_container">
+
+                    </div>
+                    <div className="column1-info_container">
+
+                    </div>
+                </div>
+                <div className="about-column_2"></div>
+            </div>
+        </div>
     )
 }
 
