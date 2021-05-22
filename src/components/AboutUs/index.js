71c62efedb66e513199200b138f6b0c3b236f882
Michael Jensen <mcjensen24@gmail.com>
2021-05-21 22:01:38 -0400

    working on margins of left about us info

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./index.css"
+import { IoRocketSharp } from 'react-icons/io5';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaTrophy } from 'react-icons/fa';
 
 const AboutUs = () => {
     return (
@@ -15,18 +18,21 @@ const AboutUs = () => {
                 <div className="about-column_1">
                     <div className="column1-info_container">
                         <div className="column1-info">
+                            <IoRocketSharp />
                             <h2>Innovators</h2>
                             <h4>You'll find that We're always in the pursuit of exceeding your expectations. We're always thinking outside the box.</h4>
                         </div>
                     </div>
                     <div className="column1-info_container">
                         <div className="column1-info">
+                            <BsFillPersonLinesFill />
                             <h2>Expert Team</h2>
                             <h4>Your project will be handled by our trusted experts. We have hand selected the most experienced professionals to work for you.</h4>
                         </div>
                     </div>
                     <div className="column1-info_container">
                         <div className="column1-info">
+                            <FaTrophy />
                             <h2>Over 20 Years of Experience</h2>
                             <h4>With over 20 years of experience You’ll find that we have the knowledge to insure your home is built to the highest of standards. </h4>
                         </div>
