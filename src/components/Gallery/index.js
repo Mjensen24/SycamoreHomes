bef4058bc52ceb2578263858967005fdda308ab5
Michael Jensen <mcjensen24@gmail.com>
2021-05-24 16:32:02 -0400

    created header for gallery page

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,7 +3,14 @@ import "./index.css"
 
 const Gallery = () => {
     return (
-        <h1>gallery</h1>
+        <div className="gallery-container">
+            <div className="gallery-row">
+                <div className="gallery-header_container">
+                    <h4>Gallery</h4>
+                    <h1>We provide weekly updates and photos for all our homes during the construction process. A representative will be available 7 days a week to answer any questions or concerns that you may have.</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
