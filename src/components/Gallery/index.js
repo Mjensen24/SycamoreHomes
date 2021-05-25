455548d4cdeae1d7c8fab6ef33406a66334871f6
Michael Jensen <mcjensen24@gmail.com>
2021-05-25 00:02:12 -0400

    last commit for the night

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./index.css"
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
 
 const Gallery = () => {
     return (
@@ -9,6 +10,50 @@ const Gallery = () => {
                     <h4>Gallery</h4>
                     <h1>We provide weekly updates and photos for all our homes during the construction process. A representative will be available 7 days a week to answer any questions or concerns that you may have.</h1>
                 </div>
+            </div>
+            <div className="gallery-tab_container">
+                <Tabs variant="soft-rounded" colorScheme="green">
+                    <TabList>
+                        <Tab>Basement</Tab>
+                        <Tab>Living Room</Tab>
+                        <Tab>Master Bedroom</Tab>
+                        <Tab>Master Bathroom</Tab>
+                        <Tab>Patio</Tab>
+                        <Tab>Kitchen</Tab>
+                        <Tab>Master Bathroom</Tab>
+                        <Tab>Patio</Tab>
+                        <Tab>Kitchen</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <p>one!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>two!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>test</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>one!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>two!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>test</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>one!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>two!</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>test</p>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
             </div>
         </div>
     )
